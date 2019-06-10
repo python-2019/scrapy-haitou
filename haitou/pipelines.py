94fdfee7fe53754841d304a8e449ede81065d45b
shanmu2020 <854731306@QQ.com>
2019-06-10 15:38:54 +0800

    .

---
+++ haitou/pipelines.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import csv
+import time
+
+from scrapy.conf import settings
+import logging
+import os
+
+
+class HaitouPipeline(object):
+
+    def open_spider(self, spider):
+        file_path = settings.get("FILE_PATH")
+        if os.path.exists(file_path):
+            os.rename(file_path, file_path.replace('.csv', "_" + str(time.time()) + '.csv'))
+        self.file = open(file_path, 'a', newline='', encoding='utf-8')
+        self.csv_writer = csv.writer(self.file)
+        headers = ['公司', '学校', '举办时间', '地点', '详情']
+        self.csv_writer.writerow(headers)
+
+    def process_item(self, item, spider):
+        item['holding_time'] = item['holding_time']
+        row = [item['company'], item['school'], item['holding_time'], item['addr'], item['href']]
+        self.csv_writer.writerow(row)
+        # logging.warning(row)
+        print(row)
+        return item
+
+    def close_spider(self, spider):
+        try:
+            self.file.closed()
+        except Exception:
+            logging.warning("\n\n======爬取完毕=====")
---
+++ haitou/spiders/HaitouSpider.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+from haitou.items import HaitouItem
+
+
+class HaitouSpider(scrapy.Spider):
+    name = 'haitou'
+    allowed_domains = ['xjh.haitou.cc']
+    host = "https://xjh.haitou.cc"
+    start_urls = [host]
+
+    def parse(self, response):
+        tr_list = response.xpath("//tr[@data-source='xjh']")
+        for tr in tr_list:
+            item = HaitouItem()
+            item['company'] = tr.xpath("./td[2]/a/div/text()").extract_first()
+            item['school'] = tr.xpath("./td[2]/a/span/text()").extract_first()
+            item['holding_time'] = tr.xpath("./td[3]/span[1]/text()").extract_first()
+            item['addr'] = tr.xpath("./td[4]/span[1]/text()").extract_first()
+            item['href'] = self.host + tr.xpath("./td[7]/a/@href").extract_first()
+            yield item
+        # 翻页
+        next_page = response.xpath("//a[contains(text(),'›')]/@href").extract_first()
+        if next_page is not None:
+            next_page_url = self.host + next_page
+            print(next_page_url)
+            yield scrapy.Request(
+                next_page_url,
+                callback=self.parse
+            )
